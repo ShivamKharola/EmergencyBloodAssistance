07451bb2fa531e328cc4991be13f5fc37df32532
Shivam Kharola <shivamkharola1@gmail.com>
2021-09-23 18:54:42 +0530

    my project

---
+++ BloodGroupVerifier.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emergencybloodassistance;
+
+import javax.swing.*;
+
+
+public class BloodGroupVerifier extends InputVerifier{
+
+    @Override
+    public boolean verify(JComponent input) {
+        JComboBox jbx = (JComboBox) input;
+         if(jbx.getSelectedIndex()!=0)
+             return true;
+         else{
+             JOptionPane.showMessageDialog(input, "Kindly select blood group", "Invalid Input", JOptionPane.ERROR_MESSAGE);
+             return false;
+         }
+        
+    }
+    
+}
